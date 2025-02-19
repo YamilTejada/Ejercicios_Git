a1f8bd7fd22bec950af67bc8ceb3a67144783aee
Yamil <yamil.tejada9.pumacayo10@gmail.com>
2025-02-19 16:25:50 -0500

    Cuarto commit

--- Avance2.py
+++ Avance2.py
@@ -1,1 +1,4 @@
-print("Avance 2")
+print("Avance 2 Avance 2 Avance 2")
+print("Avance 2 Avance 2 Avance 2")
+print("Avance 2 Avance 2 Avance 2")
+print("Avance 2 Avance 2 Avance 2")
---
+++ Avance3.py
@@ -0,0 +1,1 @@
+print("Avance 3")
